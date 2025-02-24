4a2431a8dee7562b8bb16abc273bda0686fe3a32
heozzi <hz0819@naver.com>
2025-02-24 16:43:28 +0900

    First Commit

---
+++ src/main/java/com/example/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,21 @@
+package com.example.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @EnableEurekaServer
+ * - 본 서비스가 유레카 서버로 사용이 되고,
+ * - 서비스 디스커버리 역활 담당하게 된다는 설정
+ */
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
